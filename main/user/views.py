1d851fcc1308e2e8f7a98d024527d2284c2cf2af
MahanBi <DirtyMBGamer@gmail.com>
2022-02-05 08:52:22 -0800

    add views template

--- main/user/views.py
+++ main/user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class LoginView(APIView):
+    def post(self, request): ...
+
+
+class RegisterView(APIView):
+    def post(self, request): ...
+
+
+class RegisterCodeView(APIView):
+    def post(self, request): ...
+
+
+class ChangePasswordView(APIView):
+    def post(self, request): ...
